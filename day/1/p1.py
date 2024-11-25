801f4a7584fcf1d20934aee5eb1a80452aaee090
Charlotte <charlotte-jansen@hotmail.com>
2024-11-25 21:28:24 +0100

    d1p1 works

--- day/1/p1.py
+++ day/1/p1.py
@@ -1,15 +1,31 @@
 # Code for part 1
 
+
 def main():
     return NotImplementedError()
 
+
+def sum_all_lines(input_string):
+    return sum(
+        combine_first_and_last_digits_from(line) for line in input_string.splitlines()
+    )
+
+
+def sum_all_lines_in_file(input_file_name):
+    input_file = open(input_file_name)
+    return sum(
+        combine_first_and_last_digits_from(line) for line in input_file.readlines()
+    )
+
+
 def combine_first_and_last_digits_from(line):
-    digits = '0123456789'
-    line_filtered = ''
+    digits = "0123456789"
+    line_filtered = ""
     for x in line:
         if x in digits:
             line_filtered += x
-    return int(line_filtered[0]+line_filtered[-1])
+    return int(line_filtered[0] + line_filtered[-1])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- day/1/test_p1.py
+++ day/1/test_p1.py
@@ -1,4 +1,6 @@
-from p1 import combine_first_and_last_digits_from
+from p1 import combine_first_and_last_digits_from, sum_all_lines, sum_all_lines_in_file
+from pathlib import Path
+
 
 EXAMPLE_INPUT = """\
 1abc2
@@ -20,6 +22,7 @@ def test_given_a_string_when_the_only_digits_are_in_the_first_and_last_place_com
     # Assert
     assert res == 12
 
+
 def test_given_a_string_when_the_only_digits_are_in_the_first_and_last_place_combine_them_2():
     # Structure: Arrange, Act, Assert
     # Arrange
@@ -31,9 +34,33 @@ def test_given_a_string_when_the_only_digits_are_in_the_first_and_last_place_com
     # Assert
     assert res == 34
 
+
 def test_two_digits():
     line = "pqr3stu8vwx"
 
     res = combine_first_and_last_digits_from(line)
 
     assert res == 38
+
+
+def test_more_than_two_digits():
+    line = "a1b2c3d4e5f"
+
+    res = combine_first_and_last_digits_from(line)
+
+    assert res == 15
+
+
+def test_one_digit():
+    assert combine_first_and_last_digits_from("treb7uchet") == 77
+
+
+def test_multiple_lines_in_string():
+    assert sum_all_lines(EXAMPLE_INPUT) == 142
+
+
+def test_file_input():
+    thisfile = Path(__file__)
+    thisdirectory = thisfile.parent
+    input_file_path = thisdirectory / "test_input.txt"
+    assert sum_all_lines_in_file(input_file_path) == 142
